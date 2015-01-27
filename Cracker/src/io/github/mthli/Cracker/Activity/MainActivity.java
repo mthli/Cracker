142b97825a8d4165feeb3d440ba6c43e0d1720df
newmlee <matthewlee0725@gmail.com>
2015-01-27 22:15:37 +0000

    Update

--- Cracker/src/io/github/mthli/Cracker/Activity/MainActivity.java
+++ Cracker/src/io/github/mthli/Cracker/Activity/MainActivity.java
@@ -1,19 +1,35 @@
 package io.github.mthli.Cracker.Activity;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.*;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
+import io.github.mthli.Cracker.Crash.CrashAdapter;
+import io.github.mthli.Cracker.Crash.CrashItem;
 import io.github.mthli.Cracker.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private ListView listView;
+    private CrashAdapter crashAdapter;
+    private List<CrashItem> list = new ArrayList<CrashItem>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // TODO
+        preferences = getSharedPreferences(getString(R.string.sp_cracker), MODE_PRIVATE);
+        editor = preferences.edit();
 
         initUI();
     }
@@ -41,11 +57,26 @@ public class MainActivity extends Activity {
     }
 
     private void initUI() {
-        View header = LayoutInflater.from(this).inflate(R.layout.header, null, false);
+        Switch service = (Switch) findViewById(R.id.main_header_switch);
+        service.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                editor.putBoolean(getString(R.string.sp_service), b).commit();
+            }
+        });
+        service.setChecked(preferences.getBoolean(getString(R.string.sp_service), false));
 
         ListView listView = (ListView) findViewById(R.id.main_listview);
-        listView.addHeaderView(header);
 
-        // TODO
+        crashAdapter = new CrashAdapter(this, R.layout.crash_item, list);
+        listView.setAdapter(crashAdapter);
+        crashAdapter.notifyDataSetChanged();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // TODO
+            }
+        });
     }
 }
