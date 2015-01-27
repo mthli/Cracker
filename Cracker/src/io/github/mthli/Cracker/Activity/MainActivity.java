43a26277ff5983de48dea904513936cae9804f95
newmlee <matthewlee0725@gmail.com>
2015-01-27 18:36:55 +0000

    Update

--- Cracker/src/io/github/mthli/Cracker/Activity/MainActivity.java
+++ Cracker/src/io/github/mthli/Cracker/Activity/MainActivity.java
@@ -2,15 +2,42 @@ package io.github.mthli.Cracker.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.*;
+import android.widget.ListView;
 import io.github.mthli.Cracker.R;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        View header = LayoutInflater.from(this).inflate(R.layout.header, null, false);
+
+        ListView listView = (ListView) findViewById(R.id.main_listview);
+        listView.addHeaderView(header);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.main_menu_clear:
+                // TODO
+                break;
+            case R.id.main_menu_about:
+                // TODO
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(menuItem);
     }
 }
