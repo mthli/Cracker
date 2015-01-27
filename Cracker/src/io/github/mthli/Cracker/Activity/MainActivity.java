6b29f59f5e7c3ec86473cdeb06ee5e75aaa40954
newmlee <matthewlee0725@gmail.com>
2015-01-27 16:18:20 +0000

    Add some libs.

---
+++ Cracker/src/io/github/mthli/Cracker/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.mthli.Cracker.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import io.github.mthli.Cracker.R;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 Cracker
 =======
 
-Collect UncaughtException when your android app crash.
+Collect `UncaughtException` when your android app crash.
 
 __UNDER DEVELOPMENT!!!__
 
-## License
+## How to use the source code?
+
+Just __import__ the `Cracker` folder with your __IntelliJ IDEA__.
+
+## License:
 
 _[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0 "Apache License, Version 2.0")_
